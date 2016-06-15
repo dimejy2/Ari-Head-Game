aadcb3b8b47d88e8b61b57f027a6561d548ae251
Dimeji Abidoye <dimejy2@gmail.com>
2016-06-15 15:04:28 -0400

    first commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ariGameApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the ariGameApp
+ */
+angular.module('ariGameApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name ariGameApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the ariGameApp
+ */
+angular.module('ariGameApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
